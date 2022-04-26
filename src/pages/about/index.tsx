060ea4751666b186f2d9480c6202b7aadbe84162
yangyb <yangyb@boyachain.cn>
2022-04-26 21:15:43 +0800

    add keep alive

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,14 @@
-import { Typography, Divider } from 'antd';
+import { KeepAlive } from 'umi';
+import { Typography } from 'antd';
+import { useEffect, useState } from 'react';
+
+
 const { Title, Paragraph, Text, Link } = Typography;
 
 const AboutPage = () => {
+  const [count, setCount] = useState(0);
   return (<div className='leayer'>
+    <button onClick={() => setCount(count + 1)}>{ count }</button>
     <Typography>
       <Title level={3}>开发日志</Title>
       <Title level={4}>博客改版：</Title>
@@ -14,4 +20,17 @@ const AboutPage = () => {
   </div>)
 }
 
-export default AboutPage;
+// export default AboutPage;
+
+export default () => {
+  const [ isRender, setIsRender ] = useState(false);
+  useEffect(() => {
+    setIsRender(true);
+  }, [])
+  return (<>{isRender
+    ? <KeepAlive when={true}>
+      <AboutPage />
+    </KeepAlive>
+    : <AboutPage />}
+  </>)
+};
