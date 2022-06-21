6b35f88fc7b91338ea6286cae897be2ced32bd5d
yangyb <yangyb@boyachain.cn>
2022-06-21 21:01:53 +0800

    fix deploy

--- deploy/server.js
+++ deploy/server.js
@@ -1,23 +1,65 @@
-const express = require('express');
-const app = new express();
+const http = require('http');
+const Koa = require('koa');
+const koaStatic = require('koa-static');
+const { extname, resolve } = require('path');
+const axios = require('axios');
 
-app.use(express.static('./dist'));
+const app = new Koa();
+const server = http.createServer(app.callback());
 
-app.use(async (req, res) => {
-  const render = require('./dist/umi.server');
-  res.setHeader('Content-Type', 'text/html');
+let render = null;
+app.use(async (ctx, next) => {
+
+  const ext = extname(ctx.request.path);
+  if (ext) {
+    await next(); // 走静态文件
+    return;
+  }
   
-  const context = {};
-  const { html, error, rootContainer } = await render({
-    path: req.url,
-    context,
-  });
-  
-  res.send(html);
-});
+  if (!render) {
+    render = require('./dist/umi.server');
+  }
 
-const port = 20000
-app.listen(port, (err) => {
-  err && console.log(err);
+  ctx.type = 'text/html';
+  ctx.status = 200;
+
+  const path = ctx.url;
+  
+  const { html, error, rootContainer } = await render({ path });
+
+  ctx.body = html;
+
+  if (error) {
+    console.log(error);
+    ctx.throw(500, error);
+  }
+
+  // 访问信息发送给后端
+  if (path.startsWith('/api')) return;
+
+  const data = ctx.headers;
+  for (const key in data) {
+    data[key] = data[key].replace(/\"/g, '');
+  }
+
+  axios({
+    method: 'post',
+    url: 'http://127.0.0.1:20010/api/access',
+    data: {
+      info: Object.assign(data, {
+        accessTime: Math.floor(Date.now() / 1000),
+        url: path,
+      }),
+    },
+  })
+
+})
+
+// 静态资源
+app.use(koaStatic(resolve(__dirname + '/dist')));
+
+
+const port = 20000;
+server.listen(port, () => {
   console.log(`服务已启动... http://localhost:${port}`);
-});
+})
