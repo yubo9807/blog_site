4d5ab991ebe725d973ec654e158f8eb1ee094e50
yangyb <yangyb@boyachain.cn>
2022-10-20 14:15:00 +0800

    fix: 数据防刷

--- deploy/src/middleware/intercept.ts
+++ deploy/src/middleware/intercept.ts
@@ -1,7 +1,7 @@
-import { staticExtList } from '../utils/file';
 import { Context, Next } from 'koa';
 import { extname } from 'path';
 import { getClientIP } from '../utils/network';
+import { staticExtList } from '../utils/file';
 import request, { cache } from '../utils/request';
 
 export default async(ctx: Context, next: Next) => {
@@ -18,22 +18,47 @@ export default async(ctx: Context, next: Next) => {
     cache.token = res.data.token;
   }
 
-  const [ err, res ] = await getBlacklist(cache.token);
-  if (err) ctx.throw(400, err.msg);
-
   const ip = getClientIP(ctx);
 
-  if (res.code === 508) {
-    addBlacklist(cache.token, ip);
-  }
+  const [ err, res ] = await getBlacklist(cache.token);
+  if (err) ctx.throw(400, err.msg);
 
   const blacklist = res.data;
   const index = blacklist.findIndex(val => val.ip === ip);
 
   if (index >= 0) ctx.throw(401, `您已被禁止访问，IP: ${ip}`);
 
-  await next();
+  const count = requestCount(ip);
+  if (count > 60) {
+    addBlacklist(cache.token, ip);
+  }
 
+  await next();
+}
+
+const countMap = new Map();
+
+/**
+ * 请求计数
+ * @param key  存入数据键
+ * @param time 过期时间
+ * @returns 
+ */
+function requestCount(key: string, time = 5000) {
+  const cache = countMap.get(key);
+  const initial = { value: 0, overTime: Date.now() + time }
+
+  if (!cache) {
+    countMap.set(key, initial);
+  } else {
+    if (cache.overTime - Date.now() <= 0) {  // 过期
+      countMap.set(key, Object.assign({}, initial));
+    } else {
+      countMap.set(key, Object.assign({}, cache, { value: cache.value + 1 }));
+    }
+  }
+
+  return countMap.get(key).value;
 }
 
 
