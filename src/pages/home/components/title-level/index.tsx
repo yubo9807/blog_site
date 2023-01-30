524f2f1237804f52e3a683deb1d2e5a013f3e6a9
yangyb <yangyb@boyachain.cn>
2023-01-30 09:30:41 +0800

    feat: 锚链接跳转

--- src/pages/home/components/title-level/index.tsx
+++ src/pages/home/components/title-level/index.tsx
@@ -1,8 +1,25 @@
 import './index.less';
+import { history } from 'umi';
+import { scrollTo } from '@/utils/browser';
+import { useEffect } from 'react';
+
+function roll2scrollY(id: string) {
+  history.replace({ hash: `#${id}` });
+  const ele = document.getElementById(id);
+  const top = ele && ele.offsetTop || 0;
+  scrollTo(top - 50);
+}
 
 export default ({ text }) => {
-  
+
+  useEffect(() => {
+    roll2scrollY(decodeURI(history.location.hash.slice(1)));
+  }, [])
+
   return <div className='yu-title-level'>
-    <h2>{text}</h2>
+    <h2 id={text}>
+      {text}
+      <a onClick={() => roll2scrollY(text)}>#</a>
+    </h2>
   </div>
 }
