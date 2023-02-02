da656767338dce3cdbf91366d9c712f541168de9
yangyb <yangyb@boyachain.cn>
2023-02-02 11:01:44 +0800

    feat: 防止浏览器套壳

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import '@/styles';
 import { isClient } from '@/utils/browser';
 import Toast from '@/components/custom-toast';
+import env, { PRODUCTION } from '~/config/env';
 
 if (isClient()) {
+  const ORIGIN = 'http://hpyyb.cn'
+  if (env.APP_ENV === PRODUCTION && location.origin !== ORIGIN) {
+    alert('网站被套壳，请注意安全。真实地址为：' + ORIGIN);
+  }
+
   window.addEventListener('online' , () => {
     Toast({ content: '网络恢复', icon: '&#xe65c;' });
   })
